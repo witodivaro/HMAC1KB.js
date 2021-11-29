01156edb264f3d763e9e656339aad31d9255f10e
witodivaro <witodivaro@gmail.com>
2021-11-29 22:33:10 +0300

    Add some special text for Murphy

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const path = require("path");
 const { SHA256, hexToRaw } = require("./lib/helpers");
 const { HMAC } = require("./lib/HMAC");
 
+/**
+ * Hey Murphy!
+ *
+ * Thanks for visiting this repo, it means a lot.
+ * Supporting this course for 6+ years deserves lots of respect.
+ * It really helps:
+ * Your answer for Week 2 helped me SO much with understanding PRFs.
+ *
+ * Much obliged!
+ *
+ * Thank you,
+ * Wito Divaro
+ */
+
 const filePath = path.resolve(__dirname, "files", "DAN_BONEH.mp4");
 const readStream = fs.createReadStream(filePath, { highWaterMark: 1024 });
 
